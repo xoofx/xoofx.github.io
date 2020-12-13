ba4de02daeb0e01851da8e675fddb2cdc2e23b25
Alexandre Mutel <alexandre_mutel@live.com>
2020-12-13 19:12:10 +0100

    Add support for theme switching based on preference

--- .lunet/js/xoofx.js
+++ .lunet/js/xoofx.js
@@ -19,3 +19,39 @@ var ghComment = document.getElementById("gh-comments");
 if (ghComment !== null) {
     DoGithubComments(ghComment.dataset.ghcommentid);
 }
+
+var currentXoofxTheme = "light";
+
+// Inspired by https://anduin.aiursoft.com/post/2020/3/27/bootstrap-dark-theme-minimum-style
+// Handle theme change
+const initXoofxTheme = function (e) {
+    if (e.matches) {
+        currentXoofxTheme = "dark";
+        // dark mode
+        $('.navbar-light').addClass('navbar-dark');
+        $('.navbar-light').removeClass('navbar-light');
+        $('body').addClass('bg-dark');
+        $('body').removeClass('bg-white');
+        $('.modal-content').addClass('bg-dark');
+        $('.container-fluid').addClass('bg-dark');
+        $('.list-group-item').addClass('bg-dark');
+        $('.content-wrapper').addClass('bg-dark');
+        $('.card').addClass('bg-dark');
+
+        $('.bg-light').addClass('bg-dark');
+        $('.bg-light').removeClass('bg-light');
+
+        $('.bg-white').addClass('bg-black');
+        $('.bg-white').removeClass('bg-white');
+
+        $('.bd-footer').addClass('bg-dark');
+        $('table').addClass('table-dark');
+    } else if (currentXoofxTheme == "dark") {
+        location.reload();
+    }
+}
+
+let colorSchemeQueryList = window.matchMedia('(prefers-color-scheme: dark)')
+
+initXoofxTheme(colorSchemeQueryList);
+colorSchemeQueryList.addListener(initXoofxTheme);
